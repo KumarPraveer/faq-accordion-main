47b80495e76bf9d0d00cb8378a0af761431c74d2
KumarPraveer <kumarpraveer2001@gmail.com>
2023-12-01 22:11:29 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,68 @@
+
+
+
+// plus.hide();
+$('.minus').hide();
+$(".tic-content").hide();
+
+$('.plus').on("click", () => {
+    $('.plus').hide();
+    $('.minus').show();
+    $(".tic-content").show();
+})
+
+$('.minus').on("click", () => {
+    $('.plus').show();
+    $('.minus').hide();
+    $(".tic-content").hide();
+})
+
+
+// plus.hide();
+$('.minu').hide();
+$(".tic-conten").hide();
+
+$('.plu').on("click", () => {
+    $('.plu').hide();
+    $('.minu').show();
+    $(".tic-conten").show();
+})
+
+$('.minu').on("click", () => {
+    $('.plu').show();
+    $('.minu').hide();
+    $(".tic-conten").hide();
+})
+
+
+// plus.hide();
+$('.min').hide();
+$(".tic-conte").hide();
+
+$('.pl').on("click", () => {
+    $('.pl').hide();
+    $('.min').show();
+    $(".tic-conte").show();
+})
+
+$('.min').on("click", () => {
+    $('.pl').show();
+    $('.min').hide();
+    $(".tic-conte").hide();
+})
+
+// plus.hide();
+$('.mi').hide();
+$(".tic-cont").hide();
+
+$('.p').on("click", () => {
+    $('.p').hide();
+    $('.mi').show();
+    $(".tic-cont").show();
+})
+
+$('.mi').on("click", () => {
+    $('.p').show();
+    $('.mi').hide();
+    $(".tic-cont").hide();
+})
